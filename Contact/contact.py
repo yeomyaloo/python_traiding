63414852099d265e5b43769fb5d91291d1e64021
hwiju yeom <yeom456@gmail.com>
2022-04-06 04:29:20 +0900

    8장 실전 프로젝트
    주소록 프로젝트
    1. 파일 열고 닫고
    2. 파일을 txt 파일로 저장해서 컴퓨터 다시해도 파일을 읽어들일 수 있게 하는 방법을 배웠다.
    3. 파이참(IDE)과 인터프리터 anaconda를 사용해서ㅈ 진행

---
+++ Contact/contact.py
@@ -0,0 +1,82 @@
+class Contact:
+    def __init__(self,name, phone_number, e_mail, addr):
+        self.name = name
+        self.phone_number= phone_number
+        self.e_mail=e_mail
+        self.addr =addr
+    def print_info(self):
+        print("Name: ",self.name)
+        print("Phone_number: ",self.phone_number)
+        print("E_mail: ",self.e_mail)
+        print("Address: ",self.addr)
+
+def set_contact():
+    name = input("Name: ")
+    phone_number = input("Phone_number: ")
+    e_mail = input("E_mail: ")
+    addr = input("Address: ")
+    contact = Contact(name,phone_number, e_mail, addr)
+    return contact
+
+def print_menu():
+    print("1. 연락처 입력")
+    print("2. 연락처 출력")
+    print("3. 연락처 삭제")
+    print("4. 종료")
+    menu = input("메뉴 선택: ")
+    return int(menu)
+
+def print_contact(contact_list):
+    for contact in contact_list:
+        contact.print_info()
+
+def delete_contact(contact_list,name):
+    for i, contact in enumerate(contact_list):
+        if contact.name == name:
+            del contact_list[i]
+
+def store_contact(contact_list):
+    #함수 호출 시 먼저 contact_db.txt 파일을 wt모드로 연다
+    f = open("contact_db.txt","wt")
+    for contact in contact_list:
+        f.write(contact.name + '\n')
+        f.write(contact.phone_number + '\n')
+        f.write(contact.e_mail + '\n')
+        f.write(contact.addr + '\n')
+    f.close()
+
+def load_contact(contact_list):
+    f = open("contact_db.txt", "rt")
+    lines = f.readlines()
+    num = len(lines)/4
+    num = int(num)
+
+    for i in range(num):
+        name = lines[4 * i].rstrip('\n')
+        phone = lines[4 * i +1].rstrip('\n')
+        email = lines[4 * i +2].rstrip('\n')
+        addr = lines[4 * i +3].rstrip('\n')
+        contact = Contact(name,phone,email,addr)
+        contact_list.append(contact)
+    f.close()
+
+def run():
+    contact_list = []
+    load_contact(contact_list)
+    while True:
+        menu = print_menu()
+        if menu == 1:
+            contact = set_contact()
+            contact_list.append(contact)
+        elif menu == 2:
+            print_contact(contact_list)
+        elif menu == 3:
+            name = input("연락처를 삭제하고자 하는 성함을 입력해주세요: ")
+            delete_contact(contact_list,name)
+        elif menu == 4:
+            store_contact(contact_list)
+            break
+
+
+if __name__ =="__main__":
+    run()
